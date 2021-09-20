e5344385244b01f286295effb06d9e6f25fd4ce5
Aleix <nigeon@gmail.com>
2021-09-20 14:52:01 +0200

    Init

---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import { Wallet } from 'ethers';
+
+const mnemonic = process.env.MNEMONIC;
+let accounts;
+
+if (mnemonic) {
+  accounts = {
+    mnemonic,
+  };
+} else {
+  accounts = [];
+  for (let i = 0; i < 10; i++) {
+    const wallet = Wallet.createRandom();
+    accounts.push({
+      privateKey: wallet.privateKey,
+      balance: '1000000000000000000000',
+    });
+  }
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+      forking: {
+        url: 'https://polygon-mumbai.g.alchemy.com/v2/' + process.env.ALCHEMY_TOKEN,
+      },
+      accounts: accounts,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
